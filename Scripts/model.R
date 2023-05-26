aea501b542ad8023b836b2912a1130e31ff8b6c4
Rachael Ren <rren@uw.edu>
2023-05-26 12:18:41 -0700

    relevel

--- Scripts/model.R
+++ Scripts/model.R
@@ -20,7 +20,21 @@ data <- read.csv("../Data/31119913_National2020.csv")
 
 data_subset <- data[, cols]
 
-# data_subset[data_subset == " "] <- NA
+data_subset[data_subset == " "] <- "Omit"
+
+data_subset$age = relevel(factor(data_subset$age), ref = "18-29")
+data_subset$sex = relevel(factor(data_subset$sex), ref = "Female")
+data_subset$educ18 = relevel(factor(data_subset$educ18), ref = "Never attended college")
+data_subset$income20 = relevel(factor(data_subset$income20), ref = "Omit")
+data_subset$racism20 = relevel(factor(data_subset$racism20), ref = "Omit")
+data_subset$life = relevel(factor(data_subset$life), ref = "Omit")
+data_subset$region = relevel(factor(data_subset$region), ref = "South")
+data_subset$sizeplac = relevel(factor(data_subset$sizeplac), ref = "Suburbs")
+data_subset$votemeth = relevel(factor(data_subset$votemeth), ref = "Election day")
+data_subset$abortion = relevel(factor(data_subset$abortion), ref = "Omit")
+data_subset$facemask = relevel(factor(data_subset$facemask), ref = "Omit")
+data_subset$lgbt = relevel(factor(data_subset$lgbt), ref = "Omit")
+data_subset$climatec = relevel(factor(data_subset$climatec), ref = "Omit")
 
 X <- model.matrix(pres ~ ., data_subset)[,-1]
 Y <- data_subset$pres == "Joe Biden"
@@ -63,4 +77,3 @@ summary(mod_2)
 mod_3 <- mod_step
 
 mod_step_2 <- step(mod, direction = "backward", k = log(nrow(data_subset_2)))
-
