75a5170fdaba7d75034fe2bd0ab99ceb175fd2ca
kobesar <klobby19@gmail.com>
2023-06-08 11:23:45 -0700

    formatting:

--- Scripts/vizmap.R
+++ Scripts/vizmap.R
@@ -6,20 +6,21 @@ data <- readRDS("../data/state_boot.RDS")
 
 data_state <- data.frame(biden = as.factor(unlist(data)))
 data_state$state <- str_to_lower(trimws(rownames(data_state)))
+data_state$biden <- as.factor((as.numeric(as.character(data_state$biden)) > 0.5)*1)
 rownames(data_state) <- NULL
 
 # Create the state map plot
-
 plot <- plot_usmap(data = data_state, values = "biden", color = "white", labels = TRUE, label_color = "gray100") +
   scale_fill_manual(values = c("0" = "#d23368", "1"="#2a3990"), labels = c("Trump", "Biden")) +
   labs(x = "", y = "", fill = "") +
   theme_light() +
   theme(
-    text = element_text(family = "roboto"),
+    # text = element_text(family = "roboto"),
     legend.position = c(0.5, 0.95),
     legend.direction = "horizontal",
     legend.text = element_text(size = 14),
+    axis.ticks = element_blank(),
     panel.border = element_blank(), panel.grid = element_blank(), 
     axis.text.x = element_blank(), axis.text.y = element_blank())  # Remove axis labels
 
-ggsave(plot, file = "../Plots/state_boot_map.png", width = 5, height = 5)
+ggsave(plot, file = "../Plots/state_boot_map.png", width = 8, height = 6)
--- Scripts/vizmapreal.R
+++ Scripts/vizmapreal.R
@@ -11,17 +11,17 @@ data_2020 <- data %>%
   filter(n == 1) %>% 
   select(-n)
 
-
 plot <- plot_usmap(data = data_2020, values = "biden", color = "white", labels = TRUE, label_color = "gray100") +
   scale_fill_manual(values = c("0" = "#d23368", "1"="#2a3990"), labels = c("Trump", "Biden")) +
   labs(x = "", y = "", fill = "", title = "") +
   theme_light() +
   theme(
-    text = element_text(family = "roboto"),
+    # text = element_text(family = "roboto"),
     legend.position = c(0.5, 0.95),
     legend.direction = "horizontal",
     legend.text = element_text(size = 14),
     panel.border = element_blank(), panel.grid = element_blank(), 
     axis.text.x = element_blank(), axis.text.y = element_blank())  # Remove axis labels
 
-plot
+ggsave(plot, file = "../Plots/state_map.png", width = 8, height = 6)
+
